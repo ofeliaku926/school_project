58ee9fd299f4a00318f9a11213896ffa873ecbce
ofeliaku926 <ofeliaku926@gmail.com>
2025-03-13 10:35:28 +0000

    Add 6.php

---
+++ src/6.php
@@ -0,0 +1,15 @@
+ <?php
+    // A function to generate a random string of length $len
+    function getRandomString($len) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $randomString = '';
+        for ($i = 0; $i < $len; $i++) {
+            $index = rand(0, strlen($characters) - 1);
+            $randomString .= $characters[$index];
+        }
+        return $randomString;
+    }
+    
+    // Generate a random string of length 10
+    echo getRandomString(10);
+?>
