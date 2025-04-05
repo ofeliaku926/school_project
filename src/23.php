aa2c7bf5e3322e76b940dad454d1e3e88eb27565
ofeliaku926 <ofeliaku926@gmail.com>
2025-04-05 06:33:38 +0000

    Add 23.php

---
+++ src/23.php
@@ -0,0 +1,12 @@
+<?php
+// Define variables and constants from parent directory
+define('parent_dir', 'path_to_parent_directory');
+define('constant1', 10);
+define('constant2', true);
+
+$student = array(
+    "name" => "John Doe",
+    "age" => 25,
+    "class" => "Junior"
+);
+?>
