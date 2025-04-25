1ddb742bd32fe8e320b6b04ee6c82a5114808bb6
ofeliaku926 <ofeliaku926@gmail.com>
2025-04-25 19:54:03 +0000

    Add 29.php

---
+++ src/29.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "school_project";
+
+// 创建连接
+$conn = new mysqli($servername, $username, $password, $dbname);
+// 检查连接
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+}
+echo "连接成功";
+?>
